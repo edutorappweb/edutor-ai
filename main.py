6bf6c65fb8239af5c203ef817900b854466a8bff
Edutor App <120645655+edutorappweb@users.noreply.github.com>
2024-12-11 13:44:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -133,4 +133,4 @@ async def generate_podcast(request: GetPodcastRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
